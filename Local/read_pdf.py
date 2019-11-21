f041f9e94dcc7aa47a8d460ceb5cf21cf0cd859f
xoqhdgh <xoqhdgh@localhost.localdomain>
2019-11-21 23:42:00 +0900

    arxiv 뽑아내기

---
+++ Local/read_pdf.py
@@ -0,0 +1,34 @@
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+from urllib.request import urlopen
+
+def read_pdf_file(pdfFile):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+
+    process_pdf(rsrcmgr, device, pdfFile)
+    device.close()
+
+    content = retstr.getvalue()
+    retstr.close()
+    return content
+
+pdf_file = open("/home/xoqhdgh/다운로드/publi.pdf", "rb")
+
+contents = read_pdf_file(pdf_file)
+arxiv = contents.split("arXiv:")
+b = list
+for i in arxiv[1:-1]:
+    a = i.split('.')
+    c = '.'.join(a[0:2])
+    print(c)
+
+print(b)
+
+print(type(contents))
+pdf_file.close()
