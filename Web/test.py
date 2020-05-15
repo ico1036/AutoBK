dcf4a08e8f8ca3efdb60db13760b03f4bd983c4e
xoqhdgh <xoqhdgh@localhost.localdomain>
2020-05-15 16:23:43 +0900

    PLB test file

--- Web/test.py
+++ Web/test.py
@@ -1,10 +1,32 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
-#req=requests.get('https://doi.org/10.1103/PhysRevLett.124.041803')
-req=requests.get('https://doi.org/10.1007/JHEP01(2020)036')
-html = req.text
-soup = BeautifulSoup(html,'html.parser')
-a = soup.find_all("meta",{"name":"citation_journal_title"})[0]['content']
 
-print(a)
+download_dir = "./"
+options = webdriver.ChromeOptions()
+
+profile = {"plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
+        "download.default_directory": download_dir,
+        "download.extensions_to_open": ".pdf"}
+
+options.add_experimental_option("prefs", profile)
+
+driver = webdriver.Chrome('/home/xoqhdgh/다운로드/chromedriver',chrome_options=options)
+driver.implicitly_wait(3)
+driver.get('http://kudos.knu.ac.kr/pages/index.htm')
+
+driver.maximize_window()
+
+driver.find_element_by_xpath("//a[@title='로그인 바로가기']").click()
+driver.find_element_by_id('userid').send_keys('2019226910')
+driver.find_element_by_id('passwd').send_keys('941002')
+driver.find_element_by_class_name('btn_login').click()
+driver.get('http://libproxy.knu.ac.kr/_Lib_Proxy_Url/https://doi.org/10.1016/j.physletb.2020.135425')
+driver.find_element_by_id("pdfLink").click()
+
+pdf = driver.find_element_by_xpath("//a[@aria-live='polite']")
+file_path = pdf.get_attribute('href')
+driver.get(file_path)
+
+time.sleep(3)
+driver.close()
